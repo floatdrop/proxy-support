b444470e6a69bc2bd15b6fa0660d91fffb650237
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-07-31 13:29:57 +0500

    Add test

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
 'use strict';
 var assert = require('assert');
+
+var http = require('http');
+var https = require('https');
+
+var caw = require('caw');
+var cawHttp = caw({protocol: 'http'});
+var cawHttps = caw({protocol: 'https'});
+
+var oldAgents = {
+	http: http.globalAgent,
+	https: https.globalAgent,
+};
+
 var proxySupport = require('./');
 
-it('should ', function () {
-	assert.strictEqual(proxySupport('unicorns'), 'unicorns & rainbows');
+it('should replace globalAgents', function () {
+	assert.equal(oldAgents.http, cawHttp);
+	assert.equal(oldAgents.https, cawHttps);
 });
