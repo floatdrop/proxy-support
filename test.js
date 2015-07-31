1fb58411c99097f525a57b631d03e0336f615e59
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-07-31 13:46:03 +0500

    Fix tests

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 'use strict';
 var assert = require('assert');
+var requireUncached = require('require-uncached');
 
 var http = require('http');
 var https = require('https');
@@ -9,11 +10,8 @@ var oldAgents = {
 	https: https.globalAgent,
 };
 
-var caw = require('caw');
-
-var proxySupport = require('./');
-
 it('should not replace globalAgents when there is not proxy', function () {
+	var proxySupport = requireUncached('./');
 	assert.equal(oldAgents.http, http.globalAgent);
 	assert.equal(oldAgents.https, https.globalAgent);
 });
@@ -22,10 +20,7 @@ it('should replace globalAgents if env is set', function () {
 	process.env.HTTPS_PROXY = '0.0.0.1';
 	process.env.HTTP_PROXY = '0.0.0.2';
 
-	var caw = require('caw');
-	var cawHttp = caw({protocol: 'http'});
-	var cawHttps = caw({protocol: 'https'});
-
-	assert.notEqual(oldAgents.http, cawHttp);
-	assert.notEqual(oldAgents.https, cawHttps);
+	var proxySupport = requireUncached('./');
+	assert.notEqual(oldAgents.http, http.globalAgent);
+	assert.notEqual(oldAgents.https, https.globalAgent);
 });
