80319ce552b8ab0e1963af5ed42202db1b3af1b0
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-07-31 17:10:06 +0500

    Force set agent

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 var caw = require('caw');
 var http = require('http');
 var https = require('https');
+var util = require('util');
+var url = require('url');
 
-http.globalAgent = caw({protocol: 'http'}) || http.globalAgent;
-https.globalAgent = caw({protocol: 'https'}) || https.globalAgent;
+var httpProxyAgent = caw({protocol: 'http'});
+
+if (httpProxyAgent) {
+	http.globalAgent = http.Agent.globalAgent = httpProxyAgent;
+	var _httpRequest = http.request;
+	http.request = function(options, cb) {
+		options.agent = options.agent || httpProxyAgent;
+		return _httpRequest.call(http, options, cb);
+	};
+}
+
+var httpsProxyAgent = caw({protocol: 'https'});
+
+if (httpsProxyAgent) {
+	https.globalAgent = https.Agent.globalAgent = httpsProxyAgent;
+	var _httpsRequest = https.request;
+	https.request = function (options, cb) {
+		if (typeof options === 'string') {
+			options = url.parse(options);
+		} else {
+			options = util._extend({}, options);
+		}
+		options.agent = options.agent || httpsProxyAgent;
+		return _httpsRequest.call(https, options, cb);
+	};
+}
